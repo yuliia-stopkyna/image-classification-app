d660ed3b6fd901897255b5bf3ae4d386a0966b71
yuliia-stopkyna <styopkinayuliya@gmail.com>
2023-04-25 22:45:41 +0300

    Create flask app and templates for upload and result

--- app.py
+++ app.py
@@ -1,12 +1,39 @@
-from flask import Flask
+import os
+
+import tensorflow as tf
+from flask import Flask, render_template, request
+
+from classifier import classify
 
 app = Flask(__name__)
 
+STATIC_FOLDER = "static"
+UPLOAD_FOLDER = "static/uploads/"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+flower_model = tf.keras.models.load_model(STATIC_FOLDER + "/model/flower_model.h5")
 
 
-if __name__ == '__main__':
+@app.get("/")
+def upload_page():
+    return render_template("upload_page.html")
+
+
+@app.post("/")
+def return_result():
+    image = request.files["image"]
+    image_path = os.path.join(UPLOAD_FOLDER, image.filename)
+    image.save(image_path)
+
+    predicted_label, probability = classify(flower_model, image_path)
+
+    return render_template(
+        "result_page.html",
+        predicted_label=predicted_label,
+        probability=probability,
+        image_path=f"uploads/{image.filename}",
+    )
+
+
+if __name__ == "__main__":
     app.run()
